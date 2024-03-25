774b6fdfe9dde84a76a4c4bd6660d5e9f4efa3df
thomasmsandoval <156626315+thomasmsandoval@users.noreply.github.com>
2024-03-25 10:48:39 -0700

    added sign up and login

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,18 +1,41 @@
 import axios from "axios";
+import { useState } from "react";
+
+const jwt = localStorage.getItem("jwt");
+if (jwt) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+}
+
 export function Login() {
+  const [errors, setErrors] = useState([]);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("handleSubmit");
+    setErrors([]);
     const params = new FormData(event.target);
-    axios.post("http://localhost:3000/products.json", params).then((response) => {
-      console.log(response);
-      event.target.reset();
-    });
+    axios
+      .post("http://localhost:3000/sessions.json", params)
+      .then((response) => {
+        console.log(response.data);
+        axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
+        localStorage.setItem("jwt", response.data.jwt);
+        event.target.reset();
+        window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
+      })
+      .catch((error) => {
+        console.log(error.response);
+        setErrors(["Invalid email or password"]);
+      });
   };
 
   return (
     <div id="login">
       <h1>Login</h1>
+      <ul>
+        {errors.map((error) => (
+          <li key={error}>{error}</li>
+        ))}
+      </ul>
       <form onSubmit={handleSubmit}>
         <div>
           Email: <input name="email" type="email" />
@@ -20,7 +43,7 @@ export function Login() {
         <div>
           Password: <input name="password" type="password" />
         </div>
-        <button type="submit">Submit</button>
+        <button type="submit">Login</button>
       </form>
     </div>
   );
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,18 +1,34 @@
 import axios from "axios";
+import { useState } from "react";
+
 export function Signup() {
+  const [errors, setErrors] = useState([]);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("handleSubmit");
+    setErrors([]);
     const params = new FormData(event.target);
-    axios.post("http://localhost:3000/products.json", params).then((response) => {
-      console.log(response);
-      event.target.reset();
-    });
+    axios
+      .post("http://localhost:3000/users.json", params)
+      .then((response) => {
+        console.log(response.data);
+        event.target.reset();
+        window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
+      })
+      .catch((error) => {
+        console.log(error.response.data.errors);
+        setErrors(error.response.data.errors);
+      });
   };
 
   return (
     <div id="signup">
-      <h1>Signup!</h1>
+      <h1>Signup</h1>
+      <ul>
+        {errors.map((error) => (
+          <li key={error}>{error}</li>
+        ))}
+      </ul>
       <form onSubmit={handleSubmit}>
         <div>
           Name: <input name="name" type="text" />
@@ -24,9 +40,9 @@ export function Signup() {
           Password: <input name="password" type="password" />
         </div>
         <div>
-          Password Confirmation: <input name="password_confirmation" type="password" />
+          Password confirmation: <input name="password_confirmation" type="password" />
         </div>
-        <button type="submit">Submit</button>
+        <button type="submit">Signup</button>
       </form>
     </div>
   );
